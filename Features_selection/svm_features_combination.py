121a6904bb99bfa87a7f152fdf56757af05065d6
BT <c1c4c5c6@gmail.com>
2020-01-22 13:29:24 +0800

    clean

--- Features_selection/svm_features_combination.py
+++ Features_selection/svm_features_combination.py
@@ -5,10 +5,12 @@ from configuration import DATE
 delta_X, y, tag = Preprocess(date_list=DATE, gamma_mode=True).delta_X_generator()
 sffs = SequentialFloatingForwardSelection(delta_X, y, tag, require_d=12)
 best_auc = sffs.run()
-# print(sffs.auc_verbose)
-# print(sffs.action_verbose)
-# print(best_auc)
 sffs.plot()
+
+
+print(sffs.auc_verbose)
+print(sffs.action_verbose)
+print(best_auc)
 ######################################################################################################################
 
 
